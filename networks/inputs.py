d19da442f2d4f8955645d0e67eb3f76a46643ccd
Restyled.io <commits@restyled.io>
2020-03-21 08:09:02 +0000

    Restyled by black

--- networks/inputs.py
+++ networks/inputs.py
@@ -7,48 +7,68 @@ class Inputs(object):
     def __init__(self, config):
         self.offset_inputs = {}
 
-        with tf.name_scope('inputs') as self.scope:
+        with tf.name_scope("inputs") as self.scope:
             self.global_step = tf.contrib.framework.get_or_create_global_step()
 
-            self.replay_count = auto_placeholder(tf.int32, (), 'replay_count',
-                                                 lambda memory, _: memory.count)
+            self.replay_count = auto_placeholder(
+                tf.int32, (), "replay_count", lambda memory, _: memory.count
+            )
 
             self.frames = auto_placeholder(
                 dtype=tf.uint8,
                 shape=[config.input_frames] + list(config.input_shape),
-                name='frames',
+                name="frames",
                 feed_data=lambda memory, indices: memory.frames[indices],
                 # Centre around 0, scale between [-1, 1]
-                preprocess_offset=lambda frames: (tf.to_float(frames) / 127.5) - 1)
+                preprocess_offset=lambda frames: (tf.to_float(frames) / 127.5) - 1,
+            )
 
             self.actions = auto_placeholder(
-                tf.int32, [1], 'actions',
-                lambda memory, indices: memory.actions[indices])
+                tf.int32,
+                [1],
+                "actions",
+                lambda memory, indices: memory.actions[indices],
+            )
 
             self.rewards = auto_placeholder(
-                tf.float32, [1], 'rewards',
-                lambda memory, indices: memory.rewards[indices])
+                tf.float32,
+                [1],
+                "rewards",
+                lambda memory, indices: memory.rewards[indices],
+            )
 
             self.alives = auto_placeholder(
-                tf.float32, [1], 'alives',
-                lambda memory, indices: memory.alives[indices])
+                tf.float32,
+                [1],
+                "alives",
+                lambda memory, indices: memory.alives[indices],
+            )
 
             self.discounted_rewards = auto_placeholder(
-                tf.float32, [1], 'discounted_rewards',
-                lambda memory, indices: memory.discounted_rewards[indices])
+                tf.float32,
+                [1],
+                "discounted_rewards",
+                lambda memory, indices: memory.discounted_rewards[indices],
+            )
 
             self.bootstrap_mask = auto_placeholder(
-                tf.float32, [1], 'bootstrap_mask',
-                lambda memory, indices: memory.bootstrap_mask[indices])
+                tf.float32,
+                [1],
+                "bootstrap_mask",
+                lambda memory, indices: memory.bootstrap_mask[indices],
+            )
 
             self.priority_probabilities = auto_placeholder(
-                tf.float32, [1], 'priority_probabilities',
-                lambda memory, indices: memory.priorities.probabilities(indices))
+                tf.float32,
+                [1],
+                "priority_probabilities",
+                lambda memory, indices: memory.priorities.probabilities(indices),
+            )
 
     def offset_input(self, t):
         if t not in self.offset_inputs:
             with tf.name_scope(self.scope):
-                with tf.name_scope(util.format_offset('input', t)):
+                with tf.name_scope(util.format_offset("input", t)):
                     offset_input = OffsetInput(self, t)
                     self.offset_inputs[t] = offset_input
         return self.offset_inputs[t]
@@ -60,21 +80,21 @@ def auto_placeholder(dtype, shape, name, feed_data, preprocess_offset=None):
     placeholder.required_feeds = RequiredFeeds(placeholder)
     placeholder.feed_data = feed_data
 
-    tensor = preprocess_offset(
-        placeholder) if preprocess_offset else placeholder
+    tensor = preprocess_offset(placeholder) if preprocess_offset else placeholder
 
     def offset_data(t, name):
         input_len = shape[0]
-        if not hasattr(placeholder, 'zero_offset'):
+        if not hasattr(placeholder, "zero_offset"):
             placeholder.zero_offset = tf.placeholder_with_default(
                 input_len - 1,  # If no zero_offset is given assume that t = 0
                 (),
-                name + '/zero_offset')
+                name + "/zero_offset",
+            )
 
         end = t + 1
         start = end - input_len
         zero_offset = placeholder.zero_offset
-        offset_tensor = tensor[:, start + zero_offset:end + zero_offset]
+        offset_tensor = tensor[:, start + zero_offset : end + zero_offset]
 
         input_range = np.arange(start, end)
         offset_tensor.required_feeds = RequiredFeeds(placeholder, input_range)
@@ -91,12 +111,13 @@ class OffsetInput(object):
         if t == 0:
             self.observations = inputs.frames
 
-        self.frames = inputs.frames.offset_data(t, 'frames')
-        self.action = inputs.actions.offset_data(t, 'action')
-        self.reward = inputs.rewards.offset_data(t, 'reward')
-        self.alive = inputs.alives.offset_data(t, 'alive')
+        self.frames = inputs.frames.offset_data(t, "frames")
+        self.action = inputs.actions.offset_data(t, "action")
+        self.reward = inputs.rewards.offset_data(t, "reward")
+        self.alive = inputs.alives.offset_data(t, "alive")
         self.discounted_reward = inputs.discounted_rewards.offset_data(
-            t, 'discounted_reward')
+            t, "discounted_reward"
+        )
 
 
 class RequiredFeeds(object):
@@ -140,14 +161,14 @@ class RequiredFeeds(object):
         for feed, input_range in self.feeds.items():
             offset_indices = replay_memory.offset_index(indices, input_range)
             feed_dict[feed] = feed.feed_data(replay_memory, offset_indices)
-            if hasattr(feed, 'zero_offset'):
+            if hasattr(feed, "zero_offset"):
                 feed_dict[feed.zero_offset] = -min(input_range)
 
         return feed_dict
 
     @classmethod
     def required_feeds(cls, tensor):
-        if hasattr(tensor, 'required_feeds'):
+        if hasattr(tensor, "required_feeds"):
             # Return cached result
             return tensor.required_feeds
         else:
@@ -159,6 +180,7 @@ class RequiredFeeds(object):
                 input_tensors = list(op.inputs) + list(op.control_inputs)
 
             from networks import inputs
+
             feeds = inputs.RequiredFeeds()
             for input_tensor in input_tensors:
                 feeds = feeds.merge(cls.required_feeds(input_tensor))
--- util/util.py
+++ util/util.py
@@ -5,48 +5,47 @@ import tensorflow as tf
 import time
 
 LUMINANCE_RATIOS = [0.2126, 0.7152, 0.0722]
-GRADIENT_SCALING = 'gradient_scaled_by_'
+GRADIENT_SCALING = "gradient_scaled_by_"
 
 
 def run_directory(config):
     def find_previous_run(dir):
         if os.path.isdir(dir):
-            runs = [child[4:]
-                    for child in os.listdir(dir) if child[:4] == 'run_']
+            runs = [child[4:] for child in os.listdir(dir) if child[:4] == "run_"]
             if runs:
                 return max(int(run) for run in runs)
 
         return 0
 
-    if config.run_dir == 'latest':
-        parent_dir = 'runs/%s/' % config.game
+    if config.run_dir == "latest":
+        parent_dir = "runs/%s/" % config.game
         previous_run = find_previous_run(parent_dir)
-        run_dir = parent_dir + ('run_%d' % previous_run)
+        run_dir = parent_dir + ("run_%d" % previous_run)
     elif config.run_dir:
         run_dir = config.run_dir
     else:
-        parent_dir = 'runs/%s/' % config.game
+        parent_dir = "runs/%s/" % config.game
         previous_run = find_previous_run(parent_dir)
-        run_dir = parent_dir + ('run_%d' % (previous_run + 1))
+        run_dir = parent_dir + ("run_%d" % (previous_run + 1))
 
-    if run_dir[-1] != '/':
-        run_dir += '/'
+    if run_dir[-1] != "/":
+        run_dir += "/"
 
     if not os.path.isdir(run_dir):
         os.makedirs(run_dir)
 
-    log('Checkpoint and summary directory is %s' % run_dir)
+    log("Checkpoint and summary directory is %s" % run_dir)
 
     return run_dir
 
 
 def format_offset(prefix, t):
     if t > 0:
-        return prefix + '_t_plus_' + str(t)
+        return prefix + "_t_plus_" + str(t)
     elif t == 0:
-        return prefix + '_t'
+        return prefix + "_t"
     else:
-        return prefix + '_t_minus_' + str(-t)
+        return prefix + "_t_minus_" + str(-t)
 
 
 def add_loss_summaries(total_loss):
@@ -61,8 +60,8 @@ def add_loss_summaries(total_loss):
       loss_averages_op: op for generating moving averages of losses.
     """
     # Compute the moving average of all individual losses and the total loss.
-    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-    losses = tf.get_collection('losses')
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name="avg")
+    losses = tf.get_collection("losses")
     loss_averages_op = loss_averages.apply(losses + [total_loss])
 
     # Attach a scalar summary to all individual losses and the total loss; do the
@@ -70,18 +69,18 @@ def add_loss_summaries(total_loss):
     for l in losses + [total_loss]:
         # Name each loss as '(raw)' and name the moving average version of the loss
         # as the original loss name.
-        tf.summary.scalar('loss_raw', l)
-        tf.summary.scalar('loss', loss_averages.average(l))
+        tf.summary.scalar("loss_raw", l)
+        tf.summary.scalar("loss", loss_averages.average(l))
 
     return loss_averages_op
 
 
 def scale_gradient(inputs, scale):
-    with inputs.graph.gradient_override_map({'Identity': 'scaled_gradient'}):
+    with inputs.graph.gradient_override_map({"Identity": "scaled_gradient"}):
         return tf.identity(inputs, name=GRADIENT_SCALING + str(scale))
 
 
-@tf.RegisterGradient('scaled_gradient')
+@tf.RegisterGradient("scaled_gradient")
 def scaled_gradient(op, grad):
     scale_index = op.name.rfind(GRADIENT_SCALING) + len(GRADIENT_SCALING)
     scale = float(op.name[scale_index:])
@@ -90,9 +89,10 @@ def scaled_gradient(op, grad):
 
 def log(message):
     import threading
+
     thread_id = threading.current_thread().name
-    now = datetime.strftime(datetime.now(), '%F %X')
-    print('%s %s: %s' % (now, thread_id, message))
+    now = datetime.strftime(datetime.now(), "%F %X")
+    print("%s %s: %s" % (now, thread_id, message))
 
 
 def memoize(f):
