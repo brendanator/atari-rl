c84ac15965220a4dabae04623dd689fee6cdacf2
Restyled.io <commits@restyled.io>
2020-03-23 10:28:31 +0000

    Restyled by autopep8

--- agents/training.py
+++ agents/training.py
@@ -7,95 +7,96 @@ import util
 
 
 class Trainer(object):
-  def __init__(self, config):
-    util.log('Creating network and training operations')
-    self.config = config
+    def __init__(self, config):
+        util.log('Creating network and training operations')
+        self.config = config
 
-    # Creating networks
-    factory = NetworkFactory(config)
-    self.global_step, self.train_op = factory.create_train_ops()
-    self.reset_op = factory.create_reset_target_network_op()
-    self.agents = factory.create_agents()
-    self.summary = factory.create_summary()
+        # Creating networks
+        factory = NetworkFactory(config)
+        self.global_step, self.train_op = factory.create_train_ops()
+        self.reset_op = factory.create_reset_target_network_op()
+        self.agents = factory.create_agents()
+        self.summary = factory.create_summary()
 
-  def train(self):
-    self.training = True
+    def train(self):
+        self.training = True
 
-    util.log('Creating session and loading checkpoint')
-    session = tf.train.MonitoredTrainingSession(
-        checkpoint_dir=self.config.run_dir,
-        save_summaries_steps=0,  # Summaries will be saved with train_op only
-        config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
+        util.log('Creating session and loading checkpoint')
+        session = tf.train.MonitoredTrainingSession(
+            checkpoint_dir=self.config.run_dir,
+            save_summaries_steps=0,  # Summaries will be saved with train_op only
+            config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
 
-    with session:
-      if len(self.agents) == 1:
-        self.train_agent(session, self.agents[0])
-      else:
-        self.train_threaded(session)
+        with session:
+            if len(self.agents) == 1:
+                self.train_agent(session, self.agents[0])
+            else:
+                self.train_threaded(session)
 
-    util.log('Training complete')
+        util.log('Training complete')
 
-  def train_threaded(self, session):
-    threads = []
-    for i, agent in enumerate(self.agents):
-      thread = Thread(target=self.train_agent, args=(session, agent))
-      thread.name = 'Agent-%d' % (i + 1)
-      thread.start()
-      threads.append(thread)
+    def train_threaded(self, session):
+        threads = []
+        for i, agent in enumerate(self.agents):
+            thread = Thread(target=self.train_agent, args=(session, agent))
+            thread.name = 'Agent-%d' % (i + 1)
+            thread.start()
+            threads.append(thread)
 
-    for thread in threads:
-      thread.join()
+        for thread in threads:
+            thread.join()
 
-  def train_agent(self, session, agent):
-    # Populate replay memory
-    if self.config.load_replay_memory:
-      util.log('Loading replay memory')
-      agent.replay_memory.load()
-    else:
-      util.log('Populating replay memory')
-      agent.populate_replay_memory()
+    def train_agent(self, session, agent):
+        # Populate replay memory
+        if self.config.load_replay_memory:
+            util.log('Loading replay memory')
+            agent.replay_memory.load()
+        else:
+            util.log('Populating replay memory')
+            agent.populate_replay_memory()
 
-    # Initialize step counters
-    step, steps_until_train = 0, self.config.train_period
+        # Initialize step counters
+        step, steps_until_train = 0, self.config.train_period
 
-    util.log('Starting training')
-    while self.training and step < self.config.num_steps:
-      # Start new episode
-      observation, _, done = agent.new_game()
+        util.log('Starting training')
+        while self.training and step < self.config.num_steps:
+            # Start new episode
+            observation, _, done = agent.new_game()
 
-      # Play until losing
-      while not done:
-        self.reset_target_network(session, step)
-        action = agent.action(session, step, observation)
-        observation, _, done = agent.take_action(action)
-        step += 1
-        steps_until_train -= 1
-        if done or (steps_until_train == 0):
-          step = self.train_batch(session, agent.replay_memory, step)
-          steps_until_train = self.config.train_period
+            # Play until losing
+            while not done:
+                self.reset_target_network(session, step)
+                action = agent.action(session, step, observation)
+                observation, _, done = agent.take_action(action)
+                step += 1
+                steps_until_train -= 1
+                if done or (steps_until_train == 0):
+                    step = self.train_batch(session, agent.replay_memory, step)
+                    steps_until_train = self.config.train_period
 
-      # Log episode
-      agent.log_episode(step)
+            # Log episode
+            agent.log_episode(step)
 
-    if self.config.save_replay_memory:
-      agent.replay_memory.save()
+        if self.config.save_replay_memory:
+            agent.replay_memory.save()
 
-  def reset_target_network(self, session, step):
-    if (self.reset_op and step > 0
-        and step % self.config.target_network_update_period == 0):
-      session.run(self.reset_op)
+    def reset_target_network(self, session, step):
+        if (self.reset_op and step > 0
+                and step % self.config.target_network_update_period == 0):
+            session.run(self.reset_op)
 
-  def train_batch(self, session, replay_memory, step):
-    fetches = [self.global_step, self.train_op] + self.summary.operation(step)
+    def train_batch(self, session, replay_memory, step):
+        fetches = [self.global_step, self.train_op] + \
+            self.summary.operation(step)
 
-    batch = replay_memory.sample_batch(fetches, self.config.batch_size)
-    if batch:
-      step, priorities, summary = session.run(fetches, batch.feed_dict())
-      batch.update_priorities(priorities)
-      self.summary.add_summary(summary, step)
+        batch = replay_memory.sample_batch(fetches, self.config.batch_size)
+        if batch:
+            step, priorities, summary = session.run(fetches, batch.feed_dict())
+            batch.update_priorities(priorities)
+            self.summary.add_summary(summary, step)
 
-    return step
+        return step
 
-  def stop_training(self):
-    util.log('Stopping training')
-    self.training = False
+    def stop_training(self):
+        util.log('Stopping training')
+        self.training = False
--- networks/inputs.py
+++ networks/inputs.py
@@ -4,166 +4,167 @@ import util
 
 
 class Inputs(object):
-  def __init__(self, config):
-    self.offset_inputs = {}
+    def __init__(self, config):
+        self.offset_inputs = {}
 
-    with tf.name_scope('inputs') as self.scope:
-      self.global_step = tf.contrib.framework.get_or_create_global_step()
+        with tf.name_scope('inputs') as self.scope:
+            self.global_step = tf.contrib.framework.get_or_create_global_step()
 
-      self.replay_count = auto_placeholder(tf.int32, (), 'replay_count',
-                                           lambda memory, _: memory.count)
+            self.replay_count = auto_placeholder(tf.int32, (), 'replay_count',
+                                                 lambda memory, _: memory.count)
 
-      self.frames = auto_placeholder(
-          dtype=tf.uint8,
-          shape=[config.input_frames] + list(config.input_shape),
-          name='frames',
-          feed_data=lambda memory, indices: memory.frames[indices],
-          # Centre around 0, scale between [-1, 1]
-          preprocess_offset=lambda frames: (tf.to_float(frames) / 127.5) - 1)
+            self.frames = auto_placeholder(
+                dtype=tf.uint8,
+                shape=[config.input_frames] + list(config.input_shape),
+                name='frames',
+                feed_data=lambda memory, indices: memory.frames[indices],
+                # Centre around 0, scale between [-1, 1]
+                preprocess_offset=lambda frames: (tf.to_float(frames) / 127.5) - 1)
 
-      self.actions = auto_placeholder(
-          tf.int32, [1], 'actions',
-          lambda memory, indices: memory.actions[indices])
+            self.actions = auto_placeholder(
+                tf.int32, [1], 'actions',
+                lambda memory, indices: memory.actions[indices])
 
-      self.rewards = auto_placeholder(
-          tf.float32, [1], 'rewards',
-          lambda memory, indices: memory.rewards[indices])
+            self.rewards = auto_placeholder(
+                tf.float32, [1], 'rewards',
+                lambda memory, indices: memory.rewards[indices])
 
-      self.alives = auto_placeholder(
-          tf.float32, [1], 'alives',
-          lambda memory, indices: memory.alives[indices])
+            self.alives = auto_placeholder(
+                tf.float32, [1], 'alives',
+                lambda memory, indices: memory.alives[indices])
 
-      self.discounted_rewards = auto_placeholder(
-          tf.float32, [1], 'discounted_rewards',
-          lambda memory, indices: memory.discounted_rewards[indices])
+            self.discounted_rewards = auto_placeholder(
+                tf.float32, [1], 'discounted_rewards',
+                lambda memory, indices: memory.discounted_rewards[indices])
 
-      self.bootstrap_mask = auto_placeholder(
-          tf.float32, [1], 'bootstrap_mask',
-          lambda memory, indices: memory.bootstrap_mask[indices])
+            self.bootstrap_mask = auto_placeholder(
+                tf.float32, [1], 'bootstrap_mask',
+                lambda memory, indices: memory.bootstrap_mask[indices])
 
-      self.priority_probabilities = auto_placeholder(
-          tf.float32, [1], 'priority_probabilities',
-          lambda memory, indices: memory.priorities.probabilities(indices))
+            self.priority_probabilities = auto_placeholder(
+                tf.float32, [1], 'priority_probabilities',
+                lambda memory, indices: memory.priorities.probabilities(indices))
 
-  def offset_input(self, t):
-    if t not in self.offset_inputs:
-      with tf.name_scope(self.scope):
-        with tf.name_scope(util.format_offset('input', t)):
-          offset_input = OffsetInput(self, t)
-          self.offset_inputs[t] = offset_input
-    return self.offset_inputs[t]
+    def offset_input(self, t):
+        if t not in self.offset_inputs:
+            with tf.name_scope(self.scope):
+                with tf.name_scope(util.format_offset('input', t)):
+                    offset_input = OffsetInput(self, t)
+                    self.offset_inputs[t] = offset_input
+        return self.offset_inputs[t]
 
 
 def auto_placeholder(dtype, shape, name, feed_data, preprocess_offset=None):
-  placeholder_shape = [None, None] + list(shape)[1:] if shape else shape
-  placeholder = tf.placeholder(dtype, placeholder_shape, name)
-  placeholder.required_feeds = RequiredFeeds(placeholder)
-  placeholder.feed_data = feed_data
+    placeholder_shape = [None, None] + list(shape)[1:] if shape else shape
+    placeholder = tf.placeholder(dtype, placeholder_shape, name)
+    placeholder.required_feeds = RequiredFeeds(placeholder)
+    placeholder.feed_data = feed_data
 
-  tensor = preprocess_offset(placeholder) if preprocess_offset else placeholder
+    tensor = preprocess_offset(
+        placeholder) if preprocess_offset else placeholder
 
-  def offset_data(t, name):
-    input_len = shape[0]
-    if not hasattr(placeholder, 'zero_offset'):
-      placeholder.zero_offset = tf.placeholder_with_default(
-          input_len - 1,  # If no zero_offset is given assume that t = 0
-          (),
-          name + '/zero_offset')
+    def offset_data(t, name):
+        input_len = shape[0]
+        if not hasattr(placeholder, 'zero_offset'):
+            placeholder.zero_offset = tf.placeholder_with_default(
+                input_len - 1,  # If no zero_offset is given assume that t = 0
+                (),
+                name + '/zero_offset')
 
-    end = t + 1
-    start = end - input_len
-    zero_offset = placeholder.zero_offset
-    offset_tensor = tensor[:, start + zero_offset:end + zero_offset]
+        end = t + 1
+        start = end - input_len
+        zero_offset = placeholder.zero_offset
+        offset_tensor = tensor[:, start + zero_offset:end + zero_offset]
 
-    input_range = np.arange(start, end)
-    offset_tensor.required_feeds = RequiredFeeds(placeholder, input_range)
+        input_range = np.arange(start, end)
+        offset_tensor.required_feeds = RequiredFeeds(placeholder, input_range)
 
-    return tf.reshape(offset_tensor, [-1] + shape, name)
+        return tf.reshape(offset_tensor, [-1] + shape, name)
 
-  placeholder.offset_data = offset_data
-  return placeholder
+    placeholder.offset_data = offset_data
+    return placeholder
 
 
 class OffsetInput(object):
-  def __init__(self, inputs, t):
-    # Only use for passing in full observation
-    if t == 0:
-      self.observations = inputs.frames
+    def __init__(self, inputs, t):
+        # Only use for passing in full observation
+        if t == 0:
+            self.observations = inputs.frames
 
-    self.frames = inputs.frames.offset_data(t, 'frames')
-    self.action = inputs.actions.offset_data(t, 'action')
-    self.reward = inputs.rewards.offset_data(t, 'reward')
-    self.alive = inputs.alives.offset_data(t, 'alive')
-    self.discounted_reward = inputs.discounted_rewards.offset_data(
-        t, 'discounted_reward')
+        self.frames = inputs.frames.offset_data(t, 'frames')
+        self.action = inputs.actions.offset_data(t, 'action')
+        self.reward = inputs.rewards.offset_data(t, 'reward')
+        self.alive = inputs.alives.offset_data(t, 'alive')
+        self.discounted_reward = inputs.discounted_rewards.offset_data(
+            t, 'discounted_reward')
 
 
 class RequiredFeeds(object):
-  def __init__(self, placeholder=None, time_offsets=0, feeds=None):
-    self.feeds = feeds if feeds else {}
-    if placeholder is None:
-      return
+    def __init__(self, placeholder=None, time_offsets=0, feeds=None):
+        self.feeds = feeds if feeds else {}
+        if placeholder is None:
+            return
 
-    if isinstance(time_offsets, int):
-      time_offsets = np.arange(time_offsets, time_offsets + 1)
-    self.feeds[placeholder] = time_offsets
+        if isinstance(time_offsets, int):
+            time_offsets = np.arange(time_offsets, time_offsets + 1)
+        self.feeds[placeholder] = time_offsets
 
-  def merge(self, other):
-    if not self.feeds:
-      return other
-    elif not other.feeds:
-      return self
+    def merge(self, other):
+        if not self.feeds:
+            return other
+        elif not other.feeds:
+            return self
 
-    feeds = {}
-    keys = set(self.feeds.keys()) | set(other.feeds.keys())
-    for key in keys:
-      if key in self.feeds and key in other.feeds:
-        full_range = list(self.feeds[key]) + list(other.feeds[key])
-        feeds[key] = np.arange(min(full_range), max(full_range) + 1)
-      elif key in self.feeds:
-        feeds[key] = self.feeds[key]
-      else:
-        feeds[key] = other.feeds[key]
+        feeds = {}
+        keys = set(self.feeds.keys()) | set(other.feeds.keys())
+        for key in keys:
+            if key in self.feeds and key in other.feeds:
+                full_range = list(self.feeds[key]) + list(other.feeds[key])
+                feeds[key] = np.arange(min(full_range), max(full_range) + 1)
+            elif key in self.feeds:
+                feeds[key] = self.feeds[key]
+            else:
+                feeds[key] = other.feeds[key]
 
-    return RequiredFeeds(feeds=feeds)
+        return RequiredFeeds(feeds=feeds)
 
-  def input_range(self):
-    offsets = set()
-    for value in self.feeds.values():
-      offsets |= set(value)
-    return np.arange(min(offsets), max(offsets))
+    def input_range(self):
+        offsets = set()
+        for value in self.feeds.values():
+            offsets |= set(value)
+        return np.arange(min(offsets), max(offsets))
 
-  def feed_dict(self, indices, replay_memory):
-    indices = indices.reshape(-1, 1)
-    feed_dict = {}
-    for feed, input_range in self.feeds.items():
-      offset_indices = replay_memory.offset_index(indices, input_range)
-      feed_dict[feed] = feed.feed_data(replay_memory, offset_indices)
-      if hasattr(feed, 'zero_offset'):
-        feed_dict[feed.zero_offset] = -min(input_range)
+    def feed_dict(self, indices, replay_memory):
+        indices = indices.reshape(-1, 1)
+        feed_dict = {}
+        for feed, input_range in self.feeds.items():
+            offset_indices = replay_memory.offset_index(indices, input_range)
+            feed_dict[feed] = feed.feed_data(replay_memory, offset_indices)
+            if hasattr(feed, 'zero_offset'):
+                feed_dict[feed.zero_offset] = -min(input_range)
 
-    return feed_dict
+        return feed_dict
 
-  @classmethod
-  def required_feeds(cls, tensor):
-    if hasattr(tensor, 'required_feeds'):
-      # Return cached result
-      return tensor.required_feeds
-    else:
-      # Get feeds required by all inputs
-      if isinstance(tensor, list):
-        input_tensors = tensor
-      else:
-        op = tensor if isinstance(tensor, tf.Operation) else tensor.op
-        input_tensors = list(op.inputs) + list(op.control_inputs)
+    @classmethod
+    def required_feeds(cls, tensor):
+        if hasattr(tensor, 'required_feeds'):
+            # Return cached result
+            return tensor.required_feeds
+        else:
+            # Get feeds required by all inputs
+            if isinstance(tensor, list):
+                input_tensors = tensor
+            else:
+                op = tensor if isinstance(tensor, tf.Operation) else tensor.op
+                input_tensors = list(op.inputs) + list(op.control_inputs)
 
-      from networks import inputs
-      feeds = inputs.RequiredFeeds()
-      for input_tensor in input_tensors:
-        feeds = feeds.merge(cls.required_feeds(input_tensor))
+            from networks import inputs
+            feeds = inputs.RequiredFeeds()
+            for input_tensor in input_tensors:
+                feeds = feeds.merge(cls.required_feeds(input_tensor))
 
-      # Cache results
-      if not isinstance(tensor, list):
-        tensor.required_feeds = feeds
+            # Cache results
+            if not isinstance(tensor, list):
+                tensor.required_feeds = feeds
 
-      return feeds
+            return feeds
