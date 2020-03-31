047d8275e26d117645af38cfa1606db869247108
Restyled.io <commits@restyled.io>
2020-03-31 09:01:26 +0000

    Restyled by autopep8

--- agents/exploration_bonus.py
+++ agents/exploration_bonus.py
@@ -7,58 +7,59 @@ import util
 
 
 class ExplorationBonus(object):
-  def __init__(self, config):
-    self.frame_shape = config.exploration_frame_shape
-    self.beta = config.exploration_beta
-    self.density_model = CTS(context_length=4, alphabet=set(range(8)))
+    def __init__(self, config):
+        self.frame_shape = config.exploration_frame_shape
+        self.beta = config.exploration_beta
+        self.density_model = CTS(context_length=4, alphabet=set(range(8)))
 
-  def bonus(self, observation):
-    # Get 3-bit frame
-    frame = cv2.resize(observation[-1], self.frame_shape) // 32
+    def bonus(self, observation):
+        # Get 3-bit frame
+        frame = cv2.resize(observation[-1], self.frame_shape) // 32
 
-    # Calculate pseudo count
-    prob = self.update_density_model(frame)
-    recoding_prob = self.density_model_probability(frame)
-    pseudo_count = prob * (1 - recoding_prob) / (recoding_prob - prob)
-    if pseudo_count < 0:
-      pseudo_count = 0  # Occasionally happens at start of training
+        # Calculate pseudo count
+        prob = self.update_density_model(frame)
+        recoding_prob = self.density_model_probability(frame)
+        pseudo_count = prob * (1 - recoding_prob) / (recoding_prob - prob)
+        if pseudo_count < 0:
+            pseudo_count = 0  # Occasionally happens at start of training
 
-    return self.beta / math.sqrt(pseudo_count + 0.01)
+        return self.beta / math.sqrt(pseudo_count + 0.01)
 
-  def update_density_model(self, frame):
-    return self.sum_pixel_probabilities(frame, self.density_model.update)
+    def update_density_model(self, frame):
+        return self.sum_pixel_probabilities(frame, self.density_model.update)
 
-  def density_model_probability(self, frame):
-    return self.sum_pixel_probabilities(frame, self.density_model.log_prob)
+    def density_model_probability(self, frame):
+        return self.sum_pixel_probabilities(frame, self.density_model.log_prob)
 
-  def sum_pixel_probabilities(self, frame, log_prob_func):
-    total_log_probability = 0.0
+    def sum_pixel_probabilities(self, frame, log_prob_func):
+        total_log_probability = 0.0
 
-    for y in range(frame.shape[0]):
-      for x in range(frame.shape[1]):
-        context = self.context(frame, y, x)
-        pixel = frame[y, x]
-        total_log_probability += log_prob_func(context=context, symbol=pixel)
+        for y in range(frame.shape[0]):
+            for x in range(frame.shape[1]):
+                context = self.context(frame, y, x)
+                pixel = frame[y, x]
+                total_log_probability += log_prob_func(
+                    context=context, symbol=pixel)
 
-    return math.exp(total_log_probability)
+        return math.exp(total_log_probability)
 
-  def context(self, frame, y, x):
-    """This grabs the L-shaped context around a given pixel"""
+    def context(self, frame, y, x):
+        """This grabs the L-shaped context around a given pixel"""
 
-    OUT_OF_BOUNDS = 7
-    context = [OUT_OF_BOUNDS] * 4
+        OUT_OF_BOUNDS = 7
+        context = [OUT_OF_BOUNDS] * 4
 
-    if x > 0:
-      context[3] = frame[y][x - 1]
+        if x > 0:
+            context[3] = frame[y][x - 1]
 
-    if y > 0:
-      context[2] = frame[y - 1][x]
+        if y > 0:
+            context[2] = frame[y - 1][x]
 
-      if x > 0:
-        context[1] = frame[y - 1][x - 1]
+            if x > 0:
+                context[1] = frame[y - 1][x - 1]
 
-      if x < frame.shape[1] - 1:
-        context[0] = frame[y - 1][x + 1]
+            if x < frame.shape[1] - 1:
+                context[0] = frame[y - 1][x + 1]
 
-    # The most important context symbol, 'left', comes last.
-    return context
+        # The most important context symbol, 'left', comes last.
+        return context
--- atari/atari.py
+++ atari/atari.py
@@ -7,105 +7,109 @@ from gym.envs.atari.atari_env import AtariEnv
 
 
 class Atari(object):
-  def __init__(self, summary, config):
-    self.summary = summary
+    def __init__(self, summary, config):
+        self.summary = summary
 
-    util.log('Starting %s {frameskip: %s, repeat_action_probability: %s}' %
-             (config.game, str(config.frameskip),
-              str(config.repeat_action_probability)))
+        util.log('Starting %s {frameskip: %s, repeat_action_probability: %s}' %
+                 (config.game, str(config.frameskip),
+                  str(config.repeat_action_probability)))
 
-    self.env = Atari.create_env(config)
+        self.env = Atari.create_env(config)
 
-    if isinstance(config.frameskip, int):
-      frameskip = config.frameskip
-    else:
-      frameskip = config.frameskip[1]
+        if isinstance(config.frameskip, int):
+            frameskip = config.frameskip
+        else:
+            frameskip = config.frameskip[1]
 
-    self.input_frames = config.input_frames
-    self.input_shape = config.input_shape
-    self.max_noops = config.max_noops / frameskip
-    self.render = config.render
+        self.input_frames = config.input_frames
+        self.input_shape = config.input_shape
+        self.max_noops = config.max_noops / frameskip
+        self.render = config.render
 
-    config.num_actions = self.env.action_space.n
-    self.episode = 0
+        config.num_actions = self.env.action_space.n
+        self.episode = 0
 
-  def sample_action(self):
-    return self.env.action_space.sample()
+    def sample_action(self):
+        return self.env.action_space.sample()
 
-  def reset(self):
-    """Reset the game and play some random actions"""
+    def reset(self):
+        """Reset the game and play some random actions"""
 
-    self.start_time = time.time()
-    self.episode += 1
-    self.steps = 0
-    self.score = 0
+        self.start_time = time.time()
+        self.episode += 1
+        self.steps = 0
+        self.score = 0
 
-    self.last_frame = self.env.reset()
-    if self.render: self.env.render()
-    self.frames = []
+        self.last_frame = self.env.reset()
+        if self.render:
+            self.env.render()
+        self.frames = []
 
-    for _ in range(np.random.randint(self.input_frames, self.max_noops + 1)):
-      frame, reward_, done, _ = self.env.step(0)
-      if self.render: self.env.render()
+        for _ in range(np.random.randint(self.input_frames, self.max_noops + 1)):
+            frame, reward_, done, _ = self.env.step(0)
+            if self.render:
+                self.env.render()
 
-      self.steps += 1
-      self.frames.append(self.process_frame(self.last_frame, frame))
-      self.last_frame = frame
-      self.score += reward_
+            self.steps += 1
+            self.frames.append(self.process_frame(self.last_frame, frame))
+            self.last_frame = frame
+            self.score += reward_
 
-      if done: self.reset()
+            if done:
+                self.reset()
 
-    return self.frames[-self.input_frames:], self.score, done
+        return self.frames[-self.input_frames:], self.score, done
 
-  def step(self, action):
-    frame, reward, done, _ = self.env.step(action)
-    if self.render: self.env.render()
+    def step(self, action):
+        frame, reward, done, _ = self.env.step(action)
+        if self.render:
+            self.env.render()
 
-    self.steps += 1
-    self.frames.append(self.process_frame(self.last_frame, frame))
-    self.last_frame = frame
-    self.score += reward
+        self.steps += 1
+        self.frames.append(self.process_frame(self.last_frame, frame))
+        self.last_frame = frame
+        self.score += reward
 
-    return self.frames[-self.input_frames:], reward, done
+        return self.frames[-self.input_frames:], reward, done
 
-  def process_frame(self, last_frame, current_frame):
-    # Max last 2 frames to remove flicker
-    # TODO Use ALE color_averaging instead (current causes core dump)
-    frame = np.maximum(last_frame, current_frame)
+    def process_frame(self, last_frame, current_frame):
+        # Max last 2 frames to remove flicker
+        # TODO Use ALE color_averaging instead (current causes core dump)
+        frame = np.maximum(last_frame, current_frame)
 
-    # Rescale image
-    frame = cv2.resize(frame, self.input_shape)
+        # Rescale image
+        frame = cv2.resize(frame, self.input_shape)
 
-    # Convert to greyscale
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Convert to greyscale
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    return frame
+        return frame
 
-  def log_episode(self, step):
-    duration = time.time() - self.start_time
-    steps_per_sec = self.steps / duration
+    def log_episode(self, step):
+        duration = time.time() - self.start_time
+        steps_per_sec = self.steps / duration
 
-    message = 'Episode %d, score %.0f (%d steps, %.2f secs, %.2f steps/sec)'
-    util.log(message %
-             (self.episode, self.score, self.steps, duration, steps_per_sec))
+        message = 'Episode %d, score %.0f (%d steps, %.2f secs, %.2f steps/sec)'
+        util.log(message %
+                 (self.episode, self.score, self.steps, duration, steps_per_sec))
 
-    self.summary.episode(step, self.score, self.steps, duration)
+        self.summary.episode(step, self.score, self.steps, duration)
 
-  @classmethod
-  def create_env(cls, config):
-    return FastAtariEnv(
-        game=config.game,
-        obs_type='image',
-        frameskip=config.frameskip,
-        repeat_action_probability=config.repeat_action_probability)
+    @classmethod
+    def create_env(cls, config):
+        return FastAtariEnv(
+            game=config.game,
+            obs_type='image',
+            frameskip=config.frameskip,
+            repeat_action_probability=config.repeat_action_probability)
 
-  @classmethod
-  def num_actions(cls, config):
-    return Atari.create_env(config).action_space.n
+    @classmethod
+    def num_actions(cls, config):
+        return Atari.create_env(config).action_space.n
 
 
 class FastAtariEnv(AtariEnv):
-  def _get_image(self):
-    # Don't reorder from rgb to bgr as we're converting to greyscale anyway
-    self.ale.getScreenRGB(self._buffer)  # says rgb but actually bgr
-    return self._buffer
+    def _get_image(self):
+        # Don't reorder from rgb to bgr as we're converting to greyscale anyway
+        self.ale.getScreenRGB(self._buffer)  # says rgb but actually bgr
+        return self._buffer
